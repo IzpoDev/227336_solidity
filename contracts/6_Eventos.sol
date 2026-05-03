cc6bc93b8364b915f3f17d7296c1afe1b8bacbf9
IzpoDev <ponce.1714.frp@gmail.com>
2026-05-02 21:30:46 -0500

    Subir clase 4

--- contracts/6_Eventos.sol
+++ contracts/6_Eventos.sol
@@ -5,19 +5,23 @@ import "@openzeppelin/contracts@4.9.3/utils/Counters.sol";
 
 
 contract Eventos {
-    uint256 private cantidad = 10;
 
-    event eventocambiarCantidad(address invocador, uint256 cantidad);
+    uint256 private cantidad = 10;
+    uint256 private valorAnt;
+
+    event eventocambiarCantidad(address invocador, uint256 cantidad, uint256 valorAnt);
 
 
 
     constructor(uint256 _valor){
+        valorAnt = cantidad;
        cantidad = _valor;
     }
 
     function cambiarCantidad(uint256 _cantidad) public {
+        valorAnt = cantidad;
         cantidad = _cantidad;
-        emit eventocambiarCantidad(msg.sender, _cantidad);
+        emit eventocambiarCantidad(msg.sender, _cantidad, valorAnt);
     }
 
     function obtenerCantidad() public view returns (uint256) {
