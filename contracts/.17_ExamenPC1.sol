e0c22863b341c3ed3b05ebcb4b9e7680bbe7a8d9
IzpoDev <ponce.1714.frp@gmail.com>
2026-05-16 19:45:33 -0500

    Parte 1

--- contracts/.17_ExamenPC1.sol
+++ contracts/.17_ExamenPC1.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Examen_PC1 {
-    
+import "hardhat/console.sol";
+
+contract Tienda_000227336 {
+
+    struct Elemento {
+        uint256 id;
+        string nombre;
+        uint256 costo;
+    }
+
+    Elemento[] public elementos;
+
+    constructor() {
+        console.log("Ejecutado por: 000227336 - Fabricio Adrian Ruiz Ponce");
+    }
+
+
 }
