fc121f4c220108ce835542cbc595b206c95f34c4
IzpoDev <ponce.1714.frp@gmail.com>
2026-05-16 20:05:50 -0500

    Parte 2

--- contracts/.17_ExamenPC1.sol
+++ contracts/.17_ExamenPC1.sol
@@ -16,6 +16,14 @@ contract Tienda_000227336 {
     constructor() {
         console.log("Ejecutado por: 000227336 - Fabricio Adrian Ruiz Ponce");
     }
+    address public dirContrato = 0x86cA07C6D491Ad7A535c26c5e35442f3e26e8497;
 
+    function agregarElementoTienda(uint256 _id, string memory _nombre, uint _costo) public {
+        elementos.push(Elemento(_id, _nombre, _costo));
+    }
+    function contarElementosTienda() public view returns (uint256) {
+        console.log("Ejecutado por: 000227336 - Fabricio Adrian Ruiz Ponce");
+        return elementos.length;
+    }
 
 }
